d857a46a5addf19cba167cd974e11a833ed6d68b
bigfact <bigfact@foxmail.com>
2018-11-13 16:23:25 +0800

    add: 添加组件目录结构生成脚本和代码贡献指南

---
+++ scripts/create/index.js
@@ -0,0 +1,101 @@
+/**
+ * create component directory structure
+ */
+
+const path = require('path')
+const argv = require('minimist')(process.argv.slice(2))
+
+// package path
+const packagePath = path.resolve(process.cwd(), './packages')
+
+// package name
+const packageName = typeof argv.name === 'boolean' ? '' : (argv.name || '').trim().toLowerCase()
+
+// check package name
+if (!packageName) {
+  throw new Error("Package name shouldn't be empty!")
+}
+const glob = require('glob')
+const fs = require('fs')
+const packageNames = []
+const packageNamesFilter = ['style']
+glob.sync(path.resolve(packagePath, './*')).forEach(el => {
+  if (fs.statSync(el).isDirectory()) {
+    let tmp = el.split('/')
+    let _packageName = tmp[tmp.length - 1]
+    if (_packageName === packageName) {
+      throw new Error('The package ' + packageName + ' already existed!')
+    }
+    if (packageNamesFilter.indexOf(_packageName) < 0) {
+      packageNames.push(_packageName)
+    }
+  }
+})
+packageNames.push(packageName)
+
+// create directory structure
+const template = require('./template')
+fs.mkdirSync(path.resolve(packagePath, './' + packageName))
+fs.mkdirSync(path.resolve(packagePath, './' + packageName + '/demo'))
+const data = {
+  namespace: 'kn-' + packageName,
+  demo: {
+    namespace: 'kn-' + packageName + '-demo'
+  }
+}
+template.forEach(el => {
+  let contents = el.tpl(data).split('\n')
+  contents.shift()
+  for (let i = 0; i < contents.length; i++) {
+    contents[i] = contents[i].replace('      ', '')
+  }
+  contents[contents.length - 1] = contents[contents.length - 1].trim()
+  fs.writeFileSync(path.resolve(packagePath, './' + packageName + '/' + el.output), contents.join('\n'))
+})
+
+// gererate packages index file
+let indexStr = `/**
+ * this files is gererated by scripts/create/index.js
+ */
+
+// import packages
+`
+let keys = []
+packageNames.forEach(el => {
+  let key = 'Kn' + el.substring(0, 1).toUpperCase() + el.substring(1, el.length)
+  indexStr += `import ${key} from './${el}/index.vue'\n`
+  keys.push(key)
+})
+let listStr = ''
+for (let i = 0; i < keys.length - 1; i++) {
+  listStr += keys[i] + ', '
+}
+listStr += keys[keys.length - 1]
+const packageVersion = require(path.resolve(process.cwd(), './package.json')).version
+indexStr += `
+// package version
+const version = '${packageVersion}'
+
+// init components
+const components = [${listStr}]
+const install = function (Vue) {
+  if (install.installed) return
+  components.map(component => Vue.component(component.name, component))
+}
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue)
+}
+
+// export components
+export { ${listStr} }
+
+// export default
+export default {
+  version,
+  install
+}
+`
+fs.writeFileSync(path.resolve(packagePath, './index.js'), indexStr)
+
+// complete
+console.log('The ' + packageName + ' component package already created!')
---
+++ wikis/contributing.md
@@ -0,0 +1,71 @@
+# 贡献代码指南
+
+## 开始
+
+```zsh
+# init
+git clone https://github.com/knfe/knui.git
+
+cd knui
+
+npm install
+
+# create a component named button
+npm run create -- --name button
+
+# develop
+npm run serve
+
+# visit http://localhost:8080/button/
+```
+
+## 目录结构
+
+```zsh
+|____ .editorconfig 编辑器通用配置
+|____ .eslintrc.js eslint 配置
+|____ .gitignore
+|____ .prettierrc.js 代码格式化插件配置
+|____ babel.config.js babel 配置
+|____ package.json
+|____ package-lock.json
+|____ README.md
+|____ vue.config.js vue cli 自定义配置
+|____ public 静态资源文件夹
+|____ scripts 相关开发脚本
+|____ examples 原src目录，用来存放docs的一些文件
+| |____ assets
+| |____ components
+| |____ docs 对应的组件说明演示文档
+|____ packages 所有组件存放目录
+| |____ button 按钮组件
+| | |____ demo 组件使用示例
+| | | |____ index.js 示例入口文件
+| | | |____ index.vue 示例组件
+| | | |____ index.html 示例组件 html 文件（可选）
+| | |____ zn-CN.md 组件说明（中文）
+| | |____ en-US.md 组件说明（English）
+| | |____ index.scss 组件样式文件
+| | |____ index.vue 组件源码
+| |____ index.js 统一导出文件
+```
+
+## 代码规范
+
+### JS
+
+- 代码规范遵循`eslint standard`
+
+### CSS/SCSS
+
+- [使用 BEM 规范](https://www.w3cplus.com/css/css-architecture-1.html)
+- [使用命名空间](https://www.w3cplus.com/css/css-architecture-2.html)
+
+### 组件
+
+- 每个组件必须包含
+  - demo
+    - index.js
+    - index.vue
+  - index.vue
+  - index.scss
