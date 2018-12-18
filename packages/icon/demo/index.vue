83efdaa017c03268bac311e115de5cc90af4e6bb
keno.lee <keno.lee@foxmail.com>
2018-12-18 17:45:25 +0800

    icon基础完成

---
+++ packages/icon/demo/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="kn-icon-demo">
+    <app></app>
+  </div>
+</template>
+<script>
+import app from '../index.vue'
+export default {
+  name: 'kn-icon-demo',
+  data() {
+    return {
+      msg: 'kn-icon-demo'
+    }
+  },
+  components: {
+    app
+  }
+}
+</script>
+<style lang="scss">
+.kn-icon-demo {
+}
+</style>
---
+++ packages/icon/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="kn-icon">{{msg}}</div>
+</template>
+<script>
+export default {
+  name: 'kn-icon',
+  data() {
+    return {
+      msg: 'hello kn-icon'
+    }
+  }
+}
+</script>
