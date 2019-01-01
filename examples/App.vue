73134479cf399534b070241c365e9a7ce4df455b
kerentang <1950655762@qq.com>
2019-01-01 15:54:23 +0800

    新增list页面统一开发入口文件

--- examples/App.vue
+++ examples/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <kn-button></kn-button>
     <router-view></router-view>
   </div>
 </template>
@@ -13,14 +12,10 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import "../packages/style/index.scss";
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  height: 100%;
+  font-size: 14px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- packages/index.js
+++ packages/index.js
@@ -4,15 +4,15 @@
 
 // import packages
 import KnButton from './button/index.vue'
-import KnPullRefresh from './pullRefresh/index.vue'
 import KnCell from './cell/index.vue'
 import KnIcon from './icon/index.vue'
+import KnPullRefresh from './pullRefresh/index.vue'
 
 // package version
 const version = '0.4.0'
 
 // init components
-const components = [KnButton, KnPullRefresh, KnCell, KnIcon]
+const components = [KnButton, KnCell, KnIcon, KnPullRefresh]
 const install = function (Vue) {
   if (install.installed) return
   components.map(component => Vue.component(component.name, component))
@@ -22,7 +22,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 // export components
-export { KnButton, KnPullRefresh, KnCell, KnIcon }
+export { KnButton, KnCell, KnIcon, KnPullRefresh }
 
 // export default
 export default {
